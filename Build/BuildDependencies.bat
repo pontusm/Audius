ca453f43cf853ad64e32ec1d614e9c18ac949094
pontus <pontus.munck@gmail.com>
2009-11-07 18:04:18 +0100

    Created build scripts for building dependencies.

---
+++ Build/BuildDependencies.bat
@@ -0,0 +1,3 @@
+BuildTools\NAnt\NAnt.exe -buildfile:BuildScripts\Dependencies.build.xml -D:Configuration=Debug build
+BuildTools\NAnt\NAnt.exe -buildfile:BuildScripts\Dependencies.build.xml -D:Configuration=Release build
+pause
