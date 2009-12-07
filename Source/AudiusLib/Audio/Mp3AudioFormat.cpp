3abd61a2efa211d7eca60145843c11bcc6176311
pontus <pontus.munck@gmail.com>
2009-12-07 18:15:06 +0100

    Integrated libmpg123 in build.

--- Source/AudiusLib/Audio/Mp3AudioFormat.cpp
+++ Source/AudiusLib/Audio/Mp3AudioFormat.cpp
@@ -1,10 +1,25 @@
 #include "Precompiled.h"
 #include "Mp3AudioFormat.h"
+#include "../mpg123.h"
+
+using namespace libmpg123Namespace;
 
 //==============================================================================
 #define mp3FormatName                          TRANS("Mp3 file")
 static const tchar* const mp3Extensions[] =    { T(".mp3"), 0 };
 
+class Mp3Reader : AudioFormatReader
+{
+	//mpg123_handle _handle;
+
+public:
+	Mp3Reader(InputStream* const inp)
+		: AudioFormatReader(inp, mp3FormatName)
+	{
+		//mpg123_init();
+	}
+
+};
 Mp3AudioFormat::Mp3AudioFormat()
 	: AudioFormat(mp3FormatName, (const tchar**) mp3Extensions)
 {
