fa5b6a01050e0974bdf1b153978062bd0e20afd9
pontusm <pontus.munck@gmail.com>
2009-12-17 16:46:07 +0100

    Started rewriting web client.

--- Source/AudiusLib/System/Net/WebClient.cpp
+++ Source/AudiusLib/System/Net/WebClient.cpp
@@ -22,126 +22,45 @@ private:
 public:
 	impl()
 	{
-		// Initialize curl session
-		_handle = curl_easy_init();
-		if(!_handle)
-			throw WebException(T("Failed to initialize Curl."));
-
-		// Setup error message buffer
-		memset(_errorBuffer, 0, CURL_ERROR_SIZE);
-		curl_easy_setopt(_handle, CURLOPT_ERRORBUFFER, _errorBuffer);
 	}
 	~impl()
 	{
-		close();
 	}
 
-	void close()
-	{
-		if(_handle)
-		{
-			// Close curl session
-			curl_easy_cleanup(_handle);
-			_handle = NULL;
-		}
-	}
+	CURL*	handle;
+	char	errorBuffer[CURL_ERROR_SIZE];
 
-	// Generic callback for receiving data (had to make this static to get it working with Curl)
-	static size_t receiveData(void *ptr, size_t size, size_t nmemb, void *customdata)
-	{
-		// Cast ptr to our object and pass control to its receive method
-		impl* pThis = (impl*)customdata;
-		return pThis->receiveDataInternal(ptr, size * nmemb);
-	}
+	int64	totalBytes;
 
-	// Downloads data using the default chunk size in curl (16KB)
-	void downloadChunks(const String & url, DataReceivedDelegate callback)
-	{
-		assert(callback != NULL);
-
-		_callback = callback;
-		_totalBytes = -1;
-
-		if( curl_easy_setopt(_handle, CURLOPT_URL, (const char*)url) != 0)
-			handleError();
-
-		// Setup the read callback
-		if( curl_easy_setopt(_handle, CURLOPT_WRITEFUNCTION, receiveData) != 0)
-			handleError();
-
-		// Set the custom data to be passed to the callback
-		if( curl_easy_setopt(_handle, CURLOPT_WRITEDATA, this) != 0)
-			handleError();
-
-		if( curl_easy_perform(_handle) != 0)
-			handleError();
-	}
-
-	void downloadStringCallback(String * str, shared_ptr<DataReceivedEventArgs> args)
-	{
-		(*str) += String::fromUTF8((uint8*)args->getData(), args->getBytesReceived());
-	}
-
-	void downloadStreamCallback(OutputStream * stream, shared_ptr<DataReceivedEventArgs> args)
-	{
-		// Write data to output stream
-		if(!stream->write(args->getData(), args->getBytesReceived()))
-			args->cancelTransfer = true;
-	}
-
-private:
-	// Generic receive data method (called by Curl)
-	size_t receiveDataInternal(void* ptr, uint32 receivedBytes)
-	{
-		// Is total length not known?
-		if(_totalBytes < 0)
-		{
-			// Determine content length
-			double length = -1;
-			if( curl_easy_getinfo(_handle, CURLINFO_CONTENT_LENGTH_DOWNLOAD, &length) != CURLE_OK )
-				handleError();
-			_totalBytes = (int64)length;
-		}
-
-		// Pass it on to the desired callback method
-		shared_ptr<DataReceivedEventArgs> args( new DataReceivedEventArgs( ptr, receivedBytes, _totalBytes ) );
-		_callback(args);
-
-		// If callback wants to cancel transfer we return 0 to curl to make it stop
-		if(args->cancelTransfer)
-			return 0;
-
-		// Return bytes handled to make curl continue
-		return receivedBytes;
-	}
-
-	void handleError()
-	{
-		// Wrap up the error message and throw an exception with the error
-		String errmsg(_errorBuffer);
-		throw WebException(errmsg);
-	}
-
-private:
-	CURL*	_handle;
-	char	_errorBuffer[CURL_ERROR_SIZE];
-
-	int64	_totalBytes;
-
-	DataReceivedDelegate	_callback;
+	DataReceivedDelegate	callback;
 };
 
 // ******************************
 // *** Constructor/destructor ***
 // ******************************
 WebClient::WebClient() :
-	_pimpl( new impl() )
+	vars( new impl() )
 {
+	// Initialize curl session
+	vars->handle = curl_easy_init();
+	if(!vars->handle)
+		throw WebException(T("Failed to initialize Curl."));
+
+	// Setup error message buffer
+	memset(vars->errorBuffer, 0, CURL_ERROR_SIZE);
+	curl_easy_setopt(vars->handle, CURLOPT_ERRORBUFFER, vars->errorBuffer);
 }
 
 WebClient::~WebClient()
 {
-	delete _pimpl;
+	if(vars->handle)
+	{
+		// Close curl session
+		curl_easy_cleanup(vars->handle);
+		vars->handle = NULL;
+	}
+
+	delete vars;
 }
 
 // ********************************
@@ -154,26 +73,26 @@ String WebClient::downloadString(const String & url)
 	String str;
 
 	// Bind callback to our private impl class that will take care of filling the string
-	DataReceivedDelegate callback = boost::bind(&impl::downloadStringCallback, _pimpl, &str, _1);
-	_pimpl->downloadChunks(url, callback);
+	//DataReceivedDelegate callback = boost::bind(&impl::downloadStringCallback, vars, &str, _1);
+	//vars->downloadChunks(url, callback);
 
 	return str;
 }
 
 void WebClient::downloadStream(const String & url, OutputStream & stream )
 {
-	DataReceivedDelegate callback = boost::bind(&impl::downloadStreamCallback, _pimpl, &stream, _1);
-	_pimpl->downloadChunks(url, callback);
+	//DataReceivedDelegate callback = boost::bind(&impl::downloadStreamCallback, vars, &stream, _1);
+	//vars->downloadChunks(url, callback);
 }
 
 void WebClient::downloadChunks(const String & url, DataReceivedDelegate callback)
 {
-	_pimpl->downloadChunks(url, callback);
+	//vars->downloadChunks(url, callback);
 }
 
 void WebClient::close()
 {
-	_pimpl->close();
+	//vars->close();
 }
 
 /************************************************************************/
